ac1f4354d7e1d93153eacc2064b97558b2d2ad02
Lokeswara Reddy Valipireddy <lokeswarareddy@gmail.com>
2024-04-27 19:04:59 -0700

    Implemented  books API using FastAPI

---
+++ books-api/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+from . import schemas
+from . import database
+
+
+app = FastAPI()
+
+@app.get("/")
+def get_root():
+    return "Welcome to books API"
+
+@app.get("/book/{book_id}")
+def retrieve_book(book_id:int):
+    try:
+        return database.get_book(book_id)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=404, detail=repr(e))
+
+
+
+@app.post("/book")
+def create_book(request: schemas.BookAuthorPayload):
+    database.add_book(convert_into_book_data_model(request.book), convert_into_author_data_model(request.author))
+    return "New book added " + request.book.title + " " + str(request.book.number_of_pages) \
+        + "New Author Added " + request.author.first_name + " " + request.author.last_name
+
+
+def convert_into_book_data_model(book: schemas.Book):
+    return database.Book(title=book.title, number_of_pages=book.number_of_pages)
+
+
+def convert_into_author_data_model(author: schemas.Author):
+    return database.Author(first_name=author.first_name, last_name=author.last_name)
+
+
+
